1531edfc3e20478216ec2e1f3d5c544d12916576
kousukemouri <61516863+kousukemouri@users.noreply.github.com>
2022-02-08 13:08:55 -0500

    Update fig_5_B.py

--- figure_generation/fig_5_B.py
+++ figure_generation/fig_5_B.py
@@ -10,76 +10,76 @@ import seaborn as sns
 seg = 20.86
 enhancer = 'En19'
 
-df = pd.read_table('processed_data/RESTscreen_derived_K562_emVAR.out', sep='\t')
+
+df = pd.read_table('processed_data/20201103_RESTscreen_AF_K562_emVAR.out', sep='\t')
 df = df.dropna(subset=['LogSkew'])
 df = df[df['ID'].str.contains(enhancer)]
+df = df[df['ID'].str.contains(':wP')]
+
 
 ### making a column for SNP position
-spl = df['silencer'].str.split(':wP',expand=True)
+spl = df['ID'].str.split(':wP',expand=True)
 spl.columns = ['sil','SNP_pos']
 df = pd.concat([df,spl['SNP_pos']],axis=1)
 df['SNP_pos'] = df['SNP_pos'].astype(int)
-
-
-
-#df = df.dropna(subset=['Ref_BS'])
-
 
 df = df.reset_index(drop=True)
 
 ### calc delta binding score
 ref = pd.read_table('processed_data/variants_ref_fimo.txt', sep='\t')
-spl_r = df['sequence name'].str.split(':R:wP', expand=True)
+ref = ref[(ref['start']== 91) | (ref['start']== 92)]
+ref = ref.sort_values(by='#pattern name', ascending=False) # prioritize MA0138.2
+ref = ref.drop_duplicates(subset=['sequence name'])
+spl_r = ref['sequence name'].str.split(':R:wP', expand=True)
 spl_r.columns = ['SNP', 'position']
 ref = pd.concat([ref, spl_r['SNP']],axis=1).set_index('SNP')
 ref_score = ref['score'].to_dict()
-def dicr(x):
-    return ref_score.get(x,0)
+ref_strand = ref['strand'].to_dict()
 
 alt = pd.read_table('processed_data/variants_alt_fimo.txt', sep='\t')
-spl_a = df['sequence name'].str.split(':A:wP', expand=True)
+alt = alt[(alt['start']== 91) | (alt['start']== 92)]
+alt = alt.sort_values(by='#pattern name', ascending=False)
+alt = alt.drop_duplicates(subset=['sequence name'])
+spl_a = alt['sequence name'].str.split(':A:wP', expand=True)
 spl_a.columns = ['SNP', 'position']
 alt = pd.concat([alt, spl_a['SNP']],axis=1).set_index('SNP')
 alt_score = alt['score'].to_dict()
-def dica(x):
-    return alt_score.get(x,0)
-
-df['score_ref'] = df['SNP'].apply(dicr)
-df['score_alt'] = df['SNP'].apply(dica)
-df = df.dropna(subset=['score_ref', 'score_alt'])
-df['dBS'] = df['score_alt'] - df['scpre_ref']
+alt_strand = alt['strand'].to_dict()
 
 
-### checking strand of motif
-score = pd.read_table('processed_data/ref_all_fimo.txt', sep='\t')
-MA1 = score[score['#pattern name'] == 'MA0138.1']
-MA1l = MA1['sequence name'].tolist()
-MA2 = score[score['#pattern name'] == 'MA0138.2']
-MA2l = MA2['sequence name'].tolist()
+df['score_maj'] = 0
+df['score_min'] = 0
+df['strand'] = 'NA'
 
-
-
-## flip sequences with MA0138.1 motif
-## 95th "C" of MA0138.1 = 107th "G" of MA0138.2
-
-df['rel_pos'] = 0
-df['color'] = '0.3'
-df['segment'] = 'NA'
-
+df = df.reset_index(drop=True)
 for i in range(len(df)):
-    name = df.iloc[i,3]
-    pos = df.iloc[i,18]
-    if name in MA1l:
-        df.loc[i,'rel_pos'] = 202 - pos
-    elif name in MA2l:
-        df.loc[i,'rel_pos'] = pos
-    else:
-        df.loc[i,'rel_pos'] = pos
+    id = df.loc[i,'ID']
+    s = df.loc[i,'SNP']
+    if 'R:wP' in id:
+        df.loc[i,'score_maj'] = ref_score.get(s,0)
+        df.loc[i,'score_min'] = alt_score.get(s,0)
+        df.loc[i,'strand'] = ref_strand.get(s,0)
 
-    ## change color of significant emVars
-    fdr = df.loc[i,'Skew_logFDR']
-    if float(fdr) > 1.301:
-        df.loc[i,'color'] = 'red'
+
+    elif 'A:wP' in id:
+        df.loc[i,'score_maj'] = alt_score.get(s,0)
+        df.loc[i,'score_min'] = ref_score.get(s,0)
+        df.loc[i,'strand'] = alt_strand.get(s,0)
+
+
+df = df[(df['score_maj'] > 0) & (df['score_min'] > 0)]
+df['dBS'] = df['score_min'] - df['score_maj']
+
+### orient motifs in a same direction
+df['rel_pos'] = 0
+df = df.reset_index(drop=True)
+for i in range(len(df)):
+    pos = df.loc[i,'SNP_pos']
+    str = df.loc[i,'strand']
+    if str == '+':
+        df.loc[i,'rel_pos'] = pos
+    elif str == '-':
+        df.loc[i,'rel_pos'] = 202 - pos
 
 
 ### taking absolute value of logSkew
@@ -89,14 +89,12 @@ df['dist'] = abs(101 - df['rel_pos'])
 df = df.sort_values(by='dist')
 df = df.drop_duplicates(subset=['SNP'])
 
-### option to select only emVars
-#df = df[df['Skew_logFDR'] > 1.301]
-
 df = df[(df['rel_pos'] <= 126) & (df['rel_pos'] >= 76)]
 
+
 ### separate above and below for line plots
-above = df[(df['Ref_BS'] > seg)| (df['Alt_BS'] > seg)]
-below = df[(df['Ref_BS'] < seg) & (df['Alt_BS'] < seg)]
+above = df[(df['score_min'] > seg)| (df['score_maj'] > seg)]
+below = df[(df['score_min'] < seg) & (df['score_maj'] < seg)]
 
 
 
